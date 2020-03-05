d3f2f8e4a10191e6f24cd77f8989e67e43d03b0f
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-05 15:36:57 -0300

    Prettier config

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   extends: [
     'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
     'standard'
   ],
   globals: {
