45d83af3530cc71a657e363a67d49759437493e2
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-10 20:19:12 -0300

    Sequelize configuration and initial User model

---
+++ api/src/app/models/index.ts
@@ -0,0 +1,37 @@
+'use strict'
+
+import fs from 'fs'
+import path from 'path'
+import Sequelize from 'sequelize'
+import basename = path.basename(__filename)
+import env = process.env.NODE_ENV || 'development'
+import config from (__dirname + '/../config/config.json')[env]
+import db = {}
+
+let sequelize
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config)
+}
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file))
+    db[model.name] = model
+  })
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db)
+  }
+})
+
+db.sequelize = sequelize
+db.Sequelize = Sequelize
+
+module.exports = db
