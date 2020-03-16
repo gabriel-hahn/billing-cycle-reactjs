d5b8716eebcb5ab40c10c0712125d14322497f50
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-03-16 09:56:34 -0300

    Login styles

--- app/src/pages/Login/styles.tsx
+++ app/src/pages/Login/styles.tsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Container = styled.div`
   display: flex;
@@ -23,20 +23,44 @@ export const Title = styled.h3`
   color: #4D7C8A;
   font-size: 24px;
   font-weight: normal;
+  margin-bottom: 15px;
 `;
 
 export const Input = styled.input`
-  margin: 10px;
+  margin: 5px;
   padding: 15px;
-  width: 280px;
+  width: 300px;
+  background: rgba(77, 124, 138, 0.5);
+  border: 1px solid #4D7C8A;
+  border-radius: 5px;
+  font-size: 14px;
+
+  &::-webkit-input-placeholder {
+    color: #4D7C8A;
+    font-size: 14px;
+  }
 `;
 
-export const RegisterButton = styled.button`
-  padding: 15px;
+const sharedButtonStyle = styled.button`
+  font-size: 14px;
+  text-transform: uppercase;
+  display: inline-block;
+  max-width: 300px;
+  margin: 5px;
 `;
 
-export const LoginButton = styled.button`
-  padding: 15px;
+export const RegisterButton = styled(sharedButtonStyle)`
+  background: #FFF;
+  color: #4D7C8A;
+  padding: 12px 115px;
+  border: none;
+`;
+
+export const LoginButton = styled(sharedButtonStyle)`
+  background: #4D7C8A;
+  color: #FFF;
+  padding: 12px 120px;
+  border-radius: 5px;
 `;
 
 export const FormInputs = styled.form`
