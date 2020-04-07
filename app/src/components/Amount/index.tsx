2f7675ec7e002d2a152b41545624c1002365aaab
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-07 16:08:49 -0300

    Loading spinners in amount badges

--- app/src/components/Amount/index.tsx
+++ app/src/components/Amount/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { RotateSpinner } from 'react-spinners-kit';
 
 import { formatCurrency } from '../../utils/currency';
+import { globalVariables } from '../../styles/variables';
 
 import {
   Container,
@@ -15,6 +17,7 @@ export interface AmountPropsInterface {
   description?: string;
   color: string;
   value: number;
+  loading?: boolean;
   showDate?: boolean;
 }
 
@@ -22,23 +25,30 @@ const Amount: React.FC<AmountPropsInterface> = ({
   description,
   color,
   value,
+  loading,
   showDate,
 }) => {
   const dateRange = useSelector((state: StoreInterface) => state.transactions.currentDateRange);
 
   return (
     <Container value={value} color={color}>
-      <Description>
-        {description} (R$)
-      </Description>
-      <Value>
-        {formatCurrency(value)}
-      </Value>
-      { showDate && dateRange && (
-        <Date>
-          {dateRange.startDate} - {dateRange.endDate}
-        </Date>
-      ) }
+      { loading ? (
+        <RotateSpinner size={22} color={globalVariables.mainBlue} />
+      ) : (
+        <>
+          <Description>
+            {description} (R$)
+          </Description>
+          <Value>
+            {formatCurrency(value)}
+          </Value>
+          { showDate && dateRange && (
+            <Date>
+              {dateRange.startDate} - {dateRange.endDate}
+            </Date>
+          ) }
+        </>
+      )}
     </Container>
   );
 };
