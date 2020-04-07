e5fa2ca0fe05894a60bedfb8a10ecd078978db23
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-07 11:37:09 -0300

    Initial PWA implementation - Service workers

---
+++ app/public/worker.js
@@ -0,0 +1,31 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable consistent-return */
+const CACHE_NAME = 'pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/images/*',
+  '/static/js/*.js',
+];
+
+// Install a service worker
+this.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+// Cache and return requests
+this.addEventListener('fetch', (event) => {
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
+});
+
+// Update a service worker
+this.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(caches.keys().then(cacheNames => Promise.all(
+    cacheNames.map((cacheName) => {
+      if (cacheWhitelist.indexOf(cacheName) === -1) {
+        return caches.delete(cacheName);
+      }
+    }),
+  )));
+});
