5de2837961003080c9acc0ff991aff189fdb7043
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-13 17:33:40 -0300

    Docker compose environment variables

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config({ path: `${__dirname}/.env` });
+require('dotenv').config({ path: `${__dirname}/../.env` });
 
 const express = require('express');
 const cors = require('cors');
