63007468ff51ddd96a69f105c835b147499c43e5
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-13 20:10:54 -0300

    Postgres configuration

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -1,10 +1,10 @@
 module.exports = {
   dialect: 'postgres',
-  host: 'host.docker.internal',
   // host: '127.0.0.1',
-  username: 'gabriel_postgres',
-  password: 'gabriel_postgres',
-  database: 'billing_cycle_db',
+  host: process.env.POSTGRES_HOST,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   operatorAliases: false,
   define: {
     timestamps: true,
