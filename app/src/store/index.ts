a6313e64409d639420ea7c53592c63cd66433bcb
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-04-14 08:56:19 -0300

    SagaMiddleware type fixed

--- app/src/store/index.ts
+++ app/src/store/index.ts
@@ -1,11 +1,11 @@
 import { createStore, compose, applyMiddleware } from 'redux';
-import createSagaMiddleware from 'redux-saga';
+import createSagaMiddleware, { SagaMiddleware } from 'redux-saga';
 
 import reducers from './ducks';
 import sagas from './sagas';
 
-const middlewares = [];
-const sagaMiddleware = createSagaMiddleware();
+const middlewares: SagaMiddleware[] = [];
+const sagaMiddleware: SagaMiddleware = createSagaMiddleware();
 
 middlewares.push(sagaMiddleware);
 
