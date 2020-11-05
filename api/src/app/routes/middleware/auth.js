38bcf03d8d1c4c1c4f6c96f67d59b35b1e7b7cc5
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2020-11-04 22:58:51 -0300

    User id middleware validation fixed

--- api/src/app/routes/middleware/auth.js
+++ api/src/app/routes/middleware/auth.js
@@ -13,10 +13,8 @@ module.exports = async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, process.env.APP_SECRET);
 
-    console.log(req.headers);
-
-    if (req.headers.userId && req.headers.userId !== decoded.id) {
-      return res.status(401).json({ message: 'This request is not allowed' });
+    if (req.headers.userid && req.headers.userid != decoded.id) {
+      return res.status(401).json({ message: 'Request not allowed' });
     }
 
     req.userId = decoded.id;
