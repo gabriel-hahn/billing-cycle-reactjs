5ce27a65d4825d951b4743ebcf1a8f2ca48bbd26
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2021-03-08 00:00:11 -0300

    SSL configuration fixed

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -6,9 +6,14 @@ module.exports = {
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
   operatorAliases: false,
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
   define: {
     timestamps: true,
     underscored: true,
-    underscoredAll: true,
-  },
+    underscoredAll: true
+  }
 };
